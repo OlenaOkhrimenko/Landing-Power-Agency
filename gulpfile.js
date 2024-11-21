ba3ef0156beb2d8cb4b1efb34b95441365c4c6d7
Yevhenii <Yevhenii.Boliekhovskyi@promx.net>
2024-11-21 18:44:41 +0200

    scss: use & forward update

--- gulpfile.js
+++ gulpfile.js
@@ -24,34 +24,42 @@ function html() {
 }
 
 function styles() {
-	return src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: true })
-		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(
-			autoprefixer({
-				overrideBrowserslist: browserslist,
-			}),
-		)
-		.pipe(csso())
-		.pipe(dest(cfg.outputDir + 'css', { sourcemaps: '.' }))
-		.pipe(browserSync.stream({ once: true }));
+	return (
+		src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: true })
+			.pipe(
+				sass({
+					outputStyle: 'expanded', // expanded/compressed/nested/compact
+					silenceDeprecations: ['legacy-js-api'], // Disable old API warnings
+				}).on('error', sass.logError),
+			)
+			.pipe(
+				autoprefixer({
+					overrideBrowserslist: browserslist,
+				}),
+			)
+	
+			.pipe(dest(cfg.outputDir + 'css', { sourcemaps: '.' }))
+			.pipe(browserSync.stream({ once: true }))
+	);
 }
 
 function stylesMin() {
-	return src(cfg.srcDir + '/**/*.{scss,sass}')
-		.pipe(plumber())
-		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(
-			autoprefixer({
-				overrideBrowserslist: browserslist,
-			}),
-		)
-		.pipe(
-			csso({
-				restructure: false,
-				comments: false,
-			}),
-		)
-		.pipe(dest(cfg.buildDir + 'css'));
+	return (
+		src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: true })
+			.pipe(
+				sass({
+					outputStyle: 'compressed', // expanded/compressed/nested/compact
+					silenceDeprecations: ['legacy-js-api'], // Disable old API warnings
+				}).on('error', sass.logError),
+			)
+			.pipe(
+				autoprefixer({
+					overrideBrowserslist: browserslist,
+				}),
+			)
+			// .pipe(csso())
+			.pipe(dest(cfg.outputDir + 'css'))
+	);
 }
 
 function scripts() {
