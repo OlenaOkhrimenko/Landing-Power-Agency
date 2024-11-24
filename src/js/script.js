dcd0f5235bf9d70360673836a1c8a6ebb3239991
Ім'я користувача <>
2024-11-24 19:46:10 +0200

    Swiper added _24.11.24

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 const body = document.querySelector('body');
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
+
